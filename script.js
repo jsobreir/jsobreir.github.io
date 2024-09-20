178b536fffb4e0ca77adbf87eb80ab2b303e5706
Joao Maria Sobreira De Morais Barbot <jsobreir@c2r1s4.42porto.com>
2024-09-20 21:59:48 +0100

    Added first website

---
+++ script.js
@@ -0,0 +1,6 @@
+// Select the button by its ID and add an event listener
+const button = document.getElementById('myButton');
+
+button.addEventListener('click', function() {
+    alert('Button Clicked!');
+});
