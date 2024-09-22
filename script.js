5ac6933d647a4b23138f58c34187cdf69e99396b
jbro15 <joao.barbot@outlook.com>
2024-09-22 20:31:57 +0100

    First functional version plus flask server

--- script.js
+++
@@ -1,6 +0,0 @@
-// Select the button by its ID and add an event listener
-const button = document.getElementById('myButton');
-
-button.addEventListener('click', function() {
-    alert('Button Clicked!');
-});
